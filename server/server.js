7ea077f5a368909064e6791c129771d6e70f9c57
Sohil Ansari <rsohil204@gmail.com>
2025-02-09 14:59:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://event-frontend-sqjn.onrender.com/", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
